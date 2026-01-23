0656e909853c51a6cc578a6da178c1f4365c1566
xyzz-2333 <1793546150@qq.com>
2026-01-23 22:33:26 +0800

    Changes to be committed: 	renamed:    f3.py -> f3_carrot.py 	renamed:    f4.py -> f4_wood3.py 	renamed:    f5.py -> f5_grass2.py 	renamed:    f6.py -> f6_pumpkin1.py 	new file:   f7.py

--- f6_pumpkin1.py
+++ f6_pumpkin1.py
@@ -3,6 +3,8 @@ p=0
 while True:
 	for i in range(get_world_size()):
 		for j in range(get_world_size()):
+			if get_ground_type()!= Grounds.Soil:
+				till()		
 			if p>=36:
 				harvest()
 			if can_harvest():
