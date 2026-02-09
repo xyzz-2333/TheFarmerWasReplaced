e4b0dd712f3b06d6de2525d4e809a728d016163f
xyzz-2333 <1793546150@qq.com>
2026-02-09 17:43:25 +0800

    。

--- f10_carrot&wood.py
+++ f10_carrot&wood.py
@@ -4,25 +4,24 @@ from utills import *
 def drone_func():
 	change_hat(Hats.Wizard_Hat)
 	do_a_flip()
-	while True:
-		for i in range(get_world_size()):
-			for j in range(get_world_size()):
-				if can_harvest():
-					harvest()
-				if (get_pos_y()%2==0 and get_pos_x()%2==0)or  (get_pos_y()%2==1 and get_pos_x()%2==1):
-					plant(Entities.Tree)
-				else:
-					plant(Entities.Carrot) 
-				move(S)
-			move(A)
+	#while True:
+	for i in range(get_world_size()):
+		for j in range(get_world_size()):
+			if can_harvest():
+				harvest()
+			if (get_pos_y()%2==0 and get_pos_x()%2==0)or  (get_pos_y()%2==1 and get_pos_x()%2==1):
+				plant(Entities.Tree)
+			else:
+				plant(Entities.Carrot) 
+			move(S)
+		move(A)
 #
-tillall()
-spawn_drone(drone_func())
+#tillall()
+#spawn_drone(drone_func())
 print(num_drones())
 flag=0
 while True:
-	if spawn_drone(drone_func()) and flag==0:
-		flag=1
+	spawn_drone(drone_func())
 	for i in range(get_world_size()):
 		for j in range(get_world_size()):
 			if can_harvest():
